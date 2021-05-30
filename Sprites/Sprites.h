6a36c4c4658f7987662c4dbfbb86af17b0733caf
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-05-30 16:53:56 +0100

    Created unified sprite header

---
+++ Sprites/Sprites.h
@@ -0,0 +1,16 @@
+/*
+ * Sprites.h
+ *
+ *  Created on: 30 May 2021
+ *      Author: Henry
+ */
+
+#ifndef SPRITES_SPRITES_H_
+#define SPRITES_SPRITES_H_
+
+#include "Sprites/MarioSprites.h"
+#include "Sprites/BackgroundSprites.h"
+#include "Sprites/PlatformSprites.h"
+#include "Sprites/WaterSprites.h"
+
+#endif /* SPRITES_SPRITES_H_ */
