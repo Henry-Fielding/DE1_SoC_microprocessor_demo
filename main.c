ef000d478fc151e2b2afb28eb9ae277e5b84a943
Henry-Fielding <78751620+Henry-Fielding@users.noreply.github.com>
2021-05-30 16:54:08 +0100

    Added basic game functions to main loop

--- main.c
+++ main.c
@@ -12,12 +12,14 @@
 
 #include "main.h"
 
+
 struct position {
 	signed int xNew, yNew, xPrev, yPrev;
 };
 
 main() {
 	const unsigned short* playerSprite;
+	unsigned short currentFrame[76800];
 	unsigned int timerLast = 0;
 	int i;
 
@@ -56,83 +58,110 @@ main() {
 
 	for (i = 1; i < 10; i++) {
 		platform[i].xNew = rand()%191;
-		platform[i].yNew = platform[i-1].yNew - (rand()%84 + 16);
+		platform[i].yNew = platform[i-1].yNew - (rand()%70 + 16);
 		platform[i].xPrev = platform[i].xNew;
 		platform[i].yPrev = platform[i].yNew;
 		HPS_ResetWatchdog();
 	}
 
 	while (1) {
-		if (timerLast - Timer_readTimer() >= 25000){
+		if (timerLast - Timer_readTimer() >= 16666){
 			timerLast = Timer_readTimer();
 
-//			if (*key_ptr & 0x01) xNew = xNew + 1;
-//			else if (*key_ptr & 0x02) xNew = xNew - 1;
-//			else if (*key_ptr & 0x04) xNew = xNew + 5;
-//			else if (*key_ptr & 0x08) xNew = xNew - 5;
-//
-//			pushButtons_clear();
-//
-//			if (temp == 1)		playerSprite = marioRightStand;
-//			else if (temp == 2)	playerSprite = marioRightWalk1;
-//			else if (temp == 3)	playerSprite = marioRightWalk2;
-//			else if (temp == 4)	playerSprite = marioRightWalk3;
-//
 			playerSprite = marioRightStand;
 
+			// player motion in y direction
 			player.yPrev = player.yNew;
-
 			dy = dy + 0.5; // reduce player velocity
 			player.yNew = player.yNew + dy;
-
 			// bounce on bottom of screen
 			if (player.yNew > 240) {
 				dy = -10;
 				player.yNew = 240;
-			}
-
-
-
-
-
-
-			// draw sprites
-			for (i = 0; i < 10; i++) {
-				if (platform[i].yNew >= 0) {
-					LT24_drawSprite2(background, platformSprite, platform[i].xNew, platform[i].yNew, 48, 16, platform[i].xNew - platform[i].xPrev, platform[i].yNew - platform[i].yPrev);
+			} else {
+				for (i = 0; i < 10; i++){
+					if ((player.xNew + 32 > platform[i].xNew) && (player.xNew < platform[i].xNew + 48)
+					&& (player.yNew + 64 > platform[i].yNew) && (player.yNew +64 < platform[i].yNew + 16)
+					&& (dy > 0)){
+						dy = -10;
+					}
 				}
 			}
-				LT24_drawSprite2(background, playerSprite, player.xNew, player.yNew, 32, 64, player.xNew - player.xPrev, player.yNew - player.yPrev);
-		}
 
+			// player motion in x direction
+			player.xPrev = player.xNew;
+			if (*key_ptr & 0x01) player.xNew  = player.xNew + 5;
+			else if (*key_ptr & 0x02) player.xNew  = player.xNew - 5;
+
+			// platform motion
+			if (player.yNew < 120) {
+				player.yNew = 120;
+				for (i = 0; i < 10; i++) {
+					platform[i].yPrev = platform[i].yNew;
+					platform[i].yNew = platform[i].yNew - dy;
+
+					if (platform[i].yNew > 320){
+						platform[i].xNew = rand()%191;
+						if (i == 0)	platform[i].yNew = platform[9].yNew - (rand()%84 + 16);
+						else 		platform[i].yNew = platform[i-1].yNew - (rand()%84 + 16);
+						platform[i].xPrev = platform[i].xNew;
+						platform[i].yPrev = platform[i].yNew;
+					}
+				}
+			}
+
+			// draw sprites
+			//addToFrame(currentFrame, background, 0, 0, 240, 320);
+//			memcpy (currentFrame, background, 5*sizeof(unsigned short));
+			resetFrame(currentFrame, background);
+
+			for (i = 0; i < 10; i++) {
+				if (platform[i].yNew >= 0 && platform[i].yNew < 300) {
+					//LT24_drawSprite2(background, platformSprite, platform[i].xNew, platform[i].yNew, 48, 16, platform[i].xNew - platform[i].xPrev, platform[i].yNew - platform[i].yPrev);
+					addToFrame(currentFrame, platformSprite, platform[i].xNew, platform[i].yNew, 48, 16);
+				}
+			}
+
+				addToFrame(currentFrame, playerSprite, player.xNew, player.yNew, 32, 64);
+				//LT24_drawSprite2(currentFrame, playerSprite, player.xNew, player.yNew, 32, 64, player.xNew - player.xPrev, player.yNew - player.yPrev);
+
+				LT24_copyFrameBuffer(currentFrame, 0, 0, 240, 320);
+		}
 
 			// Finally, reset the watchdog timer.
 			HPS_ResetWatchdog();
 	}
 }
 
-
-// function to clear all previous sprites are reset background
-void clearbackground (const unsigned short* background, unsigned int xorigin, unsigned int yorigin, unsigned int width, unsigned int height) {
-	unsigned int i;
-	unsigned int j;
-
-	unsigned int index = (yorigin * width) + xorigin; // set initial index position
-	unsigned int cnt = 0;
-	//const unsigned int patchsize = (width * height);
-	unsigned short patch [2048];
-
-	//copy first row
-	for (i = 0; i < height; i = i + 1){
-		for (j = 0; j < width; j = j + 1) {
-			patch[cnt] = background[index + j];
-			cnt = cnt + 1;
-		}
-		index = index + 240;
+void resetFrame (unsigned short* currentFrame, const unsigned short* background) {
+	unsigned int index;
+	for (index = 0; index < 76800; index++){
+		currentFrame[index] = background [index];
 	}
-
-	//LT24_drawSprite(patch, xorigin, yorigin, width, height);
 }
+
+void addToFrame (unsigned short* currentFrame, const unsigned short* newLayer,unsigned int xOrigin, unsigned int yOrigin, unsigned int width, unsigned int height) {
+	// at beginning of each loop start with background layer
+	// after each write add the new layer to the current Frame
+	// feed the new frame into the LCD
+	unsigned int xAddr, yAddr, newLayerIndex, currentFrameIndex;
+
+	unsigned int xEnd = xOrigin + width;
+	unsigned int yEnd = yOrigin + height;
+
+	for(yAddr = yOrigin; yAddr < yEnd ; yAddr++) {
+		for (xAddr = xOrigin; xAddr < xEnd ; xAddr++) {
+			newLayerIndex = (yAddr - yOrigin) * width + (xAddr - xOrigin);
+			currentFrameIndex = (yAddr * 240) + xAddr;
+
+			if (xAddr < xOrigin || xAddr >= xOrigin + width || yAddr < yOrigin || yAddr >= yOrigin + height || newLayer[newLayerIndex] == 0x0001) {} // do nothing
+			else currentFrame[currentFrameIndex] = newLayer[newLayerIndex];
+
+		}
+	}
+}
+
+
 
 // function to clear inputs
 // clears all set bits in the key edge register by writing 1 to them
@@ -146,3 +175,43 @@ void configure_privateTimer () {
 	Timer_setLoadValue(0xFFFFFFFF);	// load maximum value
 	Timer_setControl(224, 0, 1, 0);	// timer initialised to disabled mode
 }
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// Defunct
+//
+
+
+
+// function to clear all previous sprites are reset background
+//void clearbackground (const unsigned short* background, unsigned int xorigin, unsigned int yorigin, unsigned int width, unsigned int height) {
+//	unsigned int i;
+//	unsigned int j;
+//
+//	unsigned int index = (yorigin * width) + xorigin; // set initial index position
+//	unsigned int cnt = 0;
+//	//const unsigned int patchsize = (width * height);
+//	unsigned short patch [2048];
+//
+//	//copy first row
+//	for (i = 0; i < height; i = i + 1){
+//		for (j = 0; j < width; j = j + 1) {
+//			patch[cnt] = background[index + j];
+//			cnt = cnt + 1;
+//		}
+//		index = index + 240;
+//	}
+//
+//	//LT24_drawSprite(patch, xorigin, yorigin, width, height);
+//}
+
--- main.h
+++ main.h
@@ -13,14 +13,16 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#include <string.h>
+
 // include required header files
 #include "HPS_Watchdog/HPS_Watchdog.h"			// hardware drivers
 #include "DE1SoC_LT24/DE1SoC_LT24.h"
 #include "DE1Soc_SevenSeg/DE1Soc_SevenSeg.h"
 #include "HPS_PrivateTimer/HPS_PrivateTimer.h"
-#include "Sprites/MarioSprites.h"						// sprite bitmaps
-#include "Sprites/BackgroundSprites.h"
-#include "Sprites/PlatformSprites.h"
+
+#include "Sprites/Sprites.h"						// sprite bitmaps
+
 
 // define hardware base addresses
 volatile unsigned int *key_edge_ptr = (unsigned int *)0xFF20005C;	// KEYS 0-3 (push buttons)
@@ -35,7 +37,11 @@ volatile unsigned int *key_ptr = (unsigned int *)0xFF200050; // KEYS 0-3 (push b
 // clears all set bits in the key edge register by writing 1 to them
 void pushButtons_clear (void);
 
+void addToFrame (unsigned short* currentFrame, const unsigned short* newLayer, unsigned int xOrigin, unsigned int yOrigin, unsigned int width, unsigned int height);
+
 void clearbackground (const unsigned short*, unsigned int, unsigned int, unsigned int, unsigned int);
 
 void configure_privateTimer (void);
+
+void resetFrame (unsigned short* currentFrame, const unsigned short* background);
 #endif /* MAIN_H_ */
